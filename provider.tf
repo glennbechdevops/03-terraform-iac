6fef4f28a0cec878fb5581bc3e0cdb8d124d0fc6
glennbech <xp8fdael1>
2022-04-26 17:08:10 +0200

    Updated docs

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
     key    = "<studentnavn>/terraform-in-pipeline.state"
-    region = "eu-west-1"
+    region = "eu-north-1"
   }
 }
 
